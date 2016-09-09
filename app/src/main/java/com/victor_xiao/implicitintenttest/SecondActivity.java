bb93181baffffbf16c7e02a2a7459dea66fae5e9
VictorXiao <843731025@qq.com>
2016-09-09 16:04:46 +0800

    Initial commit

---
+++ app/src/main/java/com/victor_xiao/implicitintenttest/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.victor_xiao.implicitintenttest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Victor_Xiao on 16/09/01.
+ */
+public class SecondActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
